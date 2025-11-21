bd93078668f70e897d3bf4fe06fe87630df005f4
Bogdan Iepure <iepurebogdan@gmail.com>
2025-11-21 13:14:22 -0500

    Updated profile.py

--- profile.py
+++ profile.py
@@ -47,7 +47,7 @@ pc.defineParameter(
     description="Run deploy scripts for srsRAN_Project and srsRAN_4G",
     typ=portal.ParameterType.BOOLEAN,
     defaultValue=False,
-    advanced=True,
+    advanced=False,
 )
 
 pc.defineParameter(
